fd50d4ccce2a9213e81d64d7ac54e47dcbba6dbe
adelson70 <bittencourtadelson1@gmail.com>
2025-12-16 17:57:44 -0300

    teste

--- src/pages/Caller/index.tsx
+++ src/pages/Caller/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import './style.css'
 
 export default function Caller() {
   return (
     <div>
       <h1>Caller</h1>
+      <Link to="/">Voltar para o início</Link>
     </div>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import './style.css'
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Link to="/caller">Chamar</Link>
     </div>
   )
 }
